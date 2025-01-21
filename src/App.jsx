522baa96b6d9bd71376efe12f8e535d32df046b3
Sagar Singh <sagarsingh@Sagars-MacBook-Pro.local>
2025-01-21 23:46:52 +0530

    aboutus page added

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,15 @@ import './App.css'
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import LandingPage from './pages/LandingPage'
 import PageNotFound from './pages/PageNotFound'
+import AboutUs from './pages/AboutUs';
+
 
 function App() {
   return (
     <Router>
     <Routes>
       <Route path="/" element={<LandingPage />} />
+      <Route path="/about" element={<AboutUs />} />
       {/* <Route path="/about" element={<AboutPage />} /> */}
 
       {/* Catch-all route for 404 */}
